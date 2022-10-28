adaee0acdbaab0bb933bb1d36d4ca12cdf37552c
umaisqureshi <umaisqureshi26@gmail.com>
2022-10-28 15:57:43 +0200

    TrendingQuestions Bloc Done

--- lib/Screens/Home/Trending/trending.dart
+++ lib/Screens/Home/Trending/trending.dart
@@ -22,45 +22,45 @@ class _TrendingScreenState extends State<TrendingScreen> {
 
     return BlocProvider(
       create: (context) => TrendingQuestionsBloc(),
-      child: BlocBuilder<TrendingQuestionsBloc, TrendingQuestionsState>(
-        builder: (context, state) {
-          // if (state is TrendingQuestionsLoading) {
-          //   return Center(
-          //     child: CircularProgressIndicator(
-          //       color: Theme.of(context).primaryColor,
-          //       strokeWidth: 4,
-          //     ),
-          //   );
-          // }
-          if (state is TrendingQuestionsLoaded) {
-            return ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: Text(
-                        "Trending topics",
-                        style: GoogleFonts.nunito(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(
-                        height: size.height * 0.1,
-                        width: size.width,
-                        child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 6,
-                            itemBuilder: ((context, index) {
-                              return trendingScrollWidget(context);
-                            }))),
-                  ],
+      child: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  "Trending topics",
+                  style: GoogleFonts.nunito(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
                 ),
-                ListView.builder(
+              ),
+              SizedBox(
+                  height: size.height * 0.1,
+                  width: size.width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: ((context, index) {
+                        return trendingScrollWidget(context);
+                      }))),
+            ],
+          ),
+          BlocBuilder<TrendingQuestionsBloc, TrendingQuestionsState>(
+            builder: (context, state) {
+              // if (state is TrendingQuestionsLoading) {
+              //   return Center(
+              //     child: CircularProgressIndicator(
+              //       color: Theme.of(context).primaryColor,
+              //       strokeWidth: 4,
+              //     ),
+              //   );
+              // }
+              if (state is TrendingQuestionsLoaded) {
+                return ListView.builder(
                     physics: const BouncingScrollPhysics(),
                     shrinkWrap: true,
                     itemCount: 5,
@@ -70,33 +70,33 @@ class _TrendingScreenState extends State<TrendingScreen> {
                         name: "Umais Qureshi",
                         image: "assets/splashicon.png",
                       );
-                    })),
-              ],
-            );
-          }
-          // return Center(
-          //   child: Text(
-          //     "Trending Posts",
-          //     style: GoogleFonts.nunito(
-          //         color: Theme.of(context).primaryColor,
-          //         fontSize: 18,
-          //         fontWeight: FontWeight.bold),
-          //   ),
-          // );
+                    }));
+              }
+              // return Center(
+              //   child: Text(
+              //     "Trending Posts",
+              //     style: GoogleFonts.nunito(
+              //         color: Theme.of(context).primaryColor,
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.bold),
+              //   ),
+              // );
 
 //WE WILL WORK ON IT AFTER API'S ARE IMPLEMENTED
-          return ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: 5,
-              itemBuilder: ((context, index) {
-                return const PostWidget(
-                  email: "umaisqureshi26@gmail.com",
-                  name: "Umais Qureshi",
-                  image: "assets/splashicon.png",
-                );
-              }));
-        },
+              return ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: ((context, index) {
+                    return const PostWidget(
+                      email: "umaisqureshi26@gmail.com",
+                      name: "Umais Qureshi",
+                      image: "assets/splashicon.png",
+                    );
+                  }));
+            },
+          ),
+        ],
       ),
     );
   }
